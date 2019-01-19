b8991843d36ff3e9b92cc1570cd6ab5cfbf14efc
8o8inCodes <8o8inSquares@gmail.com>
2019-01-19 18:27:05 +0200

    added movie information

--- src/components/searchpage/MovieList.js
+++ src/components/searchpage/MovieList.js
@@ -1,14 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import MissingPoster from '../../assets/missingPoster.jpg';
 
- class MovieList extends Component {
-    render(){
-        return (
-            <div>
-                <h2>Movie List</h2>
-                {this.props.movies ? this.props.movies.map((movie) => <p key={movie.imdbID}>{movie.Title}</p>) : null}
-            </div>
-        );
-    }
+const MovieList = (props) => {
+    return (
+        <div>
+            <h2>Movie List</h2>
+            <ul>
+                {props.movies ? props.movies.map((movie) => represent(movie)) : null}
+            </ul>
+        </div>
+    );
 }
 
+const represent = (movie) =>{
+    console.log(movie.Poster);
+    const imageSrc = (movie.Poster !== "N/A") ? movie.Poster : MissingPoster
+    return (
+        <li key={movie.imdbID}>
+            <img src={imageSrc} alt="poster"/>
+            <div className="container">
+                <h4>{movie.Title}</h4>
+                <p>Type: {movie.Type}</p>
+                <p>Year: {movie.Year}</p>
+                <p><a href={`https://www.imdb.com/title/${movie.imdbID}/`}>View on IMDB</a></p>
+            </div>
+        </li>
+    )
+}
 export default MovieList;
